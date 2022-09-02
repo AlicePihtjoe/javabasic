fd78dda5df50c730c572a4534af07d5469308f63
Alice <alice.pihtjoe@khk.ee>
2022-09-02 08:38:28 -0700

    Week one exercise 9 done

---
+++ week1/Ex9.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class Ex9 {
+    public static void main(String[] args) {
+        int first = 9;
+        int second = 4;
+
+        double division = first / (double)second;
+
+        System.out.println("Nine divided by four equals " + division + ".");
+    }
+}
+
