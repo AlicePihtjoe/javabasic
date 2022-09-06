ff4aaa9e98e1f0ca6961c63a467ce5dc47924b2b
Alice <alice.pihtjoe@khk.ee>
2022-09-06 05:51:43 -0700

    Week one exercise 16 done

---
+++ week1/Ex1.java
@@ -0,0 +1,10 @@
+package week1;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        String firstName = "Alice";
+        String lastName = "Pihtjõe";
+
+        System.out.println(firstName + " " + lastName);
+    }
+}
