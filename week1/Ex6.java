0a4147d7df0d75d47e76b084f2249ae575410458
Alice <alice.pihtjoe@khk.ee>
2022-09-09 03:59:16 -0700

    Week 1 ex 6 done.

---
+++ week1/Ex6.java
@@ -0,0 +1,13 @@
+package week1;
+
+public class Ex6 {
+
+    public static void main(String[] args) {
+
+        int a = 2000;
+        int b = 3000;
+
+
+        System.out.println("2000 + 3000 = " + Integer.sum(a, b));
+    }
+}
