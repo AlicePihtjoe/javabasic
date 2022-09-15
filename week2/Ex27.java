a000b6f48a94673c2b96c23b6233cb9cd68a7a84
Alice <alice.pihtjoe@khk.ee>
2022-09-15 04:06:48 -0700

    Week 2 ex 27 done.

---
+++ week2/Ex27.java
@@ -0,0 +1,13 @@
+package week2;
+
+public class Ex27 {
+    public static void main(String[] args){
+
+        int oneToHundred = 100;
+
+        for (int i = 1; i <= oneToHundred; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
