8d283faecedb0d147ea1898a5eae496b159912b8
Alice <alice.pihtjoe@khk.ee>
2022-09-15 00:38:28 -0700

    Week 6 ex 101 - Library search by all done.

--- week6/Library.java
+++ week6/Library.java
@@ -1,4 +1,66 @@
 package week6;
 
+
+import java.util.ArrayList;
+
 public class Library {
+
+    private ArrayList<Book> books;
+
+    // constructor
+    public Library() {
+        this.books = new ArrayList<Book>();
+    }
+
+    //meetod addbook
+    public void addBook(Book newBook){
+        this.books.add(newBook);
+    }
+
+    //meetod printbook
+    public void printBooks(){
+        for (Book book: this.books) {
+            System.out.println(book);
+        }
+
+
+    }
+
+    public ArrayList<Book> searchByTitle(String title) {
+        ArrayList<Book> found = new ArrayList<Book>();
+
+        for (Book book: this.books) {
+            if(book.title().contains(title.trim())){
+                found.add(book);
+            }
+
+        }
+
+        return found;
+   }
+    public ArrayList<Book> searchByPublisher(String publisher) {
+        ArrayList<Book> found = new ArrayList<Book>();
+
+        for (Book book: this.books) {
+            if(book.publisher().contains(publisher.trim())){
+                found.add(book);
+            }
+
+        }
+
+        return found;
+    }
+
+    public ArrayList<Book> searchByYear(int year) {
+        ArrayList<Book> found = new ArrayList<Book>();
+
+        for (Book book: this.books) {
+            if(book.year() == year){
+                found.add(book);
+            }
+
+        }
+
+        return found;
+    }
 }
