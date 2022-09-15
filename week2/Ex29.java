72ad14b62811400b3f43758784b9d80c6cdf940e
Alice <alice.pihtjoe@khk.ee>
2022-09-15 04:24:23 -0700

    Week 2 ex 29 done.

---
+++ week2/Ex29.java
@@ -0,0 +1,16 @@
+package week2;
+
+public class Ex29 {
+
+    public static void main(String[] args) {
+
+        int number = 100;
+
+        for(int i = 1; i<= number; i++) {
+
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
