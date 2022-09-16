31c806f554451ef542c239965c5ac65c369506fd
Alice <alice.pihtjoe@khk.ee>
2022-09-16 03:32:10 -0700

    Week 7 - Exercise 7.3: Listing All Words done

---
+++ week1/Ex7.java
@@ -0,0 +1,12 @@
+package week1;
+
+public class Ex7 {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 1450;
+
+        System.out.println("10 * 1450 = " + (a * b));
+    }
+}
