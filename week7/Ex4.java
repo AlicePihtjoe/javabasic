39168347d28cb55e12e21f0a1ad6f3165d9e3715
Alice <alice.pihtjoe@khk.ee>
2022-09-16 01:56:32 -0700

    Week 7 ex 4.6 -  Container:  done and tested.

--- week7/Ex4.java
+++ week7/Ex4.java
@@ -2,15 +2,23 @@ package week7;
 
 public class Ex4 {
 
-    public static void main(String[] args) {
-        Thing book = new Thing("Happiness in Three Steps", 2);
-        Thing mobile = new Thing("Nokia 3210", 1);
+        public static void main(String[] args) {
+            Thing book = new Thing("Happiness in Three Steps", 2);
+            Thing mobile = new Thing("Nokia 3210", 1);
+            Thing brick = new Thing("Brick", 4);
 
-        System.out.println("Book name: " + book.getName());
-        System.out.println("Book weight: " + book.getWeight());
+            Suitcase tomsCase = new Suitcase(10);
+            tomsCase.addThing(book);
+            tomsCase.addThing(mobile);
 
-        System.out.println("Book: " + book);
-        System.out.println("Mobile: " + mobile);
+            Suitcase georgesCase = new Suitcase(10);
+            georgesCase.addThing(brick);
 
+            Container container = new Container(1000);
+            container.addSuitcase(tomsCase);
+            container.addSuitcase(georgesCase);
+
+            System.out.println(container);
+        }
     }
-}
+
