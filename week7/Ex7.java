d003449ef91223a5c22a24488ea2682dcb6e171b
Alice <alice.pihtjoe@khk.ee>
2022-09-16 04:57:06 -0700

    Week 8 katsetused - book

--- week7/Ex7.java
+++ week7/Ex7.java
@@ -12,3 +12,5 @@ public static void main(String[] args) {
     ui.start();
 }
 }
+
+//commiti jaoks kommentaar
---
+++ week8/Ex38wk8.java
@@ -0,0 +1,9 @@
+package week8;
+
+public class Ex38wk8 {
+public static void main(String[] args) {
+
+    Book objectBook = new Book("Raamat", 2000);
+System.out.println(objectBook);
+  }
+}
