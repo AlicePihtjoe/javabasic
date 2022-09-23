d27106b385ef2451dd4878cc0ff7dac6279e498e
Alice <alice.pihtjoe@khk.ee>
2022-09-23 00:33:19 -0700

    week9 ex 20.2: Thermometer

---
+++ week9/Ex20.java
@@ -0,0 +1,20 @@
+package week9;
+
+import week9.measurements.ConstantSensor;
+import week9.measurements.Sensor;
+import week9.measurements.Thermometer;
+
+public class Ex20 {
+
+    public static void main(String[] args) throws IllegalAccessException {
+        Sensor kumpula = new Thermometer();
+        kumpula.on();
+        System.out.println("the temperature in Kumpula is "+kumpula.measure() + " degrees");
+
+        kumpula.off();
+        System.out.println("the temperature in Kumpula is "+kumpula.measure() + " degrees");
+
+        Sensor kaisaniemi = new Thermometer();
+        Sensor helsinkiVantaa = new Thermometer();
+    }
+}
